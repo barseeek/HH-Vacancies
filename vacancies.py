c9a254af46d12890380b3db84e745fb87f1da039
Nikita <barskii.no@outlook.com>
2023-11-21 13:07:24 +0300

    Get vacancy stats from HH and SJ

--- vacancies.py
+++ vacancies.py
@@ -1,12 +1,97 @@
 import requests
+import time
+import os
+from dotenv import load_dotenv
 
 
-def predict_rub_salary(vacancy):
+def get_hh_vacancies():
+    url = "https://api.hh.ru/vacancies"
+    lang_vacancies = {}
+    programming_languages = ["Python", "JavaScript", "Java",
+                             "Ruby", "C++", "PHP"]
+    for language in programming_languages:
+        vacancy_name = "Программист {0}".format(language)
+        current_page = 0
+        pages_number = 1
+        vacancies_found = 0
+        vacancies_salary = []
+        while current_page < pages_number:
+            params = {
+                "text": vacancy_name,
+                "area": 1,
+                "only_with_salary": True,
+                "page": current_page
+            }
+            response = requests.get(url, params=params)
+            response.raise_for_status()
+            time.sleep(1)  # для избегания превышения количества запросов к Api
+            pages_number = response.json()["pages"]
+            current_page += 1
+            print(current_page, pages_number)
+            for vacancy in response.json()["items"]:
+                rub_salary = predict_rub_salary_hh(vacancy)
+                if rub_salary:
+                    vacancies_salary.append(rub_salary)
+        vacancies_found += response.json()["found"]
+        lang_vacancies.update(
+            {
+                language: {
+                    "vacancies_found": vacancies_found,
+                    "vacancies_processed": len(vacancies_salary),
+                    "average_salary": int(sum(vacancies_salary)
+                                          / len(vacancies_salary))
+                }
+            }
+        )
+    print(lang_vacancies)
+
+
+def get_superjob_vacancies():
+    load_dotenv()
+    url = "https://api.superjob.ru/2.0/vacancies/"
+    headers = {
+        "X-Api-App-Id": os.environ["SUPERJOB_KEY"]
+    }
+
+    lang_vacancies = {}
+    programming_languages = ["python", "JavaScript", "C#",
+                             "Go", "C++", "PHP"]
+    for language in programming_languages:
+        current_page = 0
+        more_pages = True
+        vacancies_found = 0
+        vacancies_salary = []
+        while more_pages is True:
+            params = {
+                "catalogues": 48,
+                "town": 4,
+                "keyword": language,
+                "count": 100,
+                "page": current_page
+            }
+            response = requests.get(url, headers=headers, params=params)
+            response.raise_for_status()
+            more_pages = response.json()["more"]
+            for vacancy in response.json()["objects"]:
+                rub_salary = predict_rub_salary_sj(vacancy)
+                if rub_salary:
+                    vacancies_salary.append(rub_salary)
+        vacancies_found += response.json()["total"]
+        lang_vacancies.update(
+            {
+                language: {
+                    "vacancies_found": vacancies_found,
+                    "vacancies_processed": len(vacancies_salary),
+                    "average_salary": int(sum(vacancies_salary)
+                                          / len(vacancies_salary))
+                }
+            }
+        )
+    print(lang_vacancies)
+
+
+def predict_rub_salary(salary_from, salary_to):
     rub_salary = 0
-    salary_from = vacancy["salary"].get("from")
-    salary_to = vacancy["salary"].get("to")
-    if vacancy["salary"].get("currency") != "RUR":
-        return None
     if salary_from:
         if salary_to:
             rub_salary = (salary_from + salary_to) / 2
@@ -17,35 +102,29 @@ def predict_rub_salary(vacancy):
     return int(rub_salary)
 
 
+def predict_rub_salary_hh(vacancy):
+    if vacancy["salary"].get("currency") != "RUR":
+        return None
+    else:
+        return predict_rub_salary(
+            vacancy["salary"].get("from"),
+            vacancy["salary"].get("to")
+        )
+
+
+def predict_rub_salary_sj(vacancy):
+    if vacancy.get("currency") != "rub":
+        return None
+    else:
+        return predict_rub_salary(
+            vacancy.get("payment_from"),
+            vacancy.get("payment_to")
+        )
+
+
 def main():
-    url = "https://api.hh.ru/vacancies"
-    lang_vacancies = {}
-    programming_languages = ["Python", "JavaScript", "Java",
-                             "Ruby", "PHP", "C++"]
-    for language in programming_languages:
-        vacancy_name = "Программист {0}".format(language)
-        params = {
-            "text": vacancy_name,
-            "area": 1,
-            "only_with_salary": True
-        }
-        response = requests.get(url, params=params)
-        vacancies_salary = []
-        for vacancy in response.json()["items"]:
-            rub_salary = predict_rub_salary(vacancy)
-            if rub_salary:
-                vacancies_salary.append(rub_salary)
-        lang_vacancies.update(
-            {
-                language: {
-                    "vacancies_found": response.json()["found"],
-                    "vacancies_processed": len(vacancies_salary),
-                    "average_salary": int(sum(vacancies_salary)
-                                          / len(vacancies_salary))
-                }
-            }
-        )        
-    print(lang_vacancies)
+    get_hh_vacancies()
+    get_superjob_vacancies()
 
 
 if __name__ == "__main__":
